2f3d547feca2ce1754b25b6fc61d28fe2d949e18
RhysWood <rhyswoody@gmail.com>
2021-11-20 23:24:17 +0000

    refactored code to take a callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 const request = require('request');
-const breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if(!error){
-    const data = JSON.parse(body);
-    if (data[0] === undefined){
-      console.log(`We have no information on the ${breed} breed!`)
-      return
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        callback(`We do not have any information on ${breedName}`, null);
+        return;
+      } else {
+        callback(null, data[0].description);
+        return;
+      }
     } else {
-      console.log(data[0].description);
-      return
+      callback(`Something went wrong! ${error}`);
+      return;
     }
-  } else {
-    console.log(`Something went wrong! ${error}`);
-    return;
-  };
-})
+  });
+};
+
+module.exports = { fetchBreedDescription };
